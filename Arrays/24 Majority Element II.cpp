184ae7b5841280e81a126e81b9cfc516ed6c8148
Rishita Maheshwari <rishitamaheshwari07@gmail.com>
2024-07-25 17:18:35 +0530

    Create 24 Majority Element II.cpp

---
+++ Arrays/24 Majority Element II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+      map<int , int>mp;
+      int n= nums.size()/3;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        vector<int>ans;
+        for( auto &it :mp)
+        {
+           if(it.second>n)
+           {
+               ans.push_back(it.first);
+           }
+        }
+        return ans;
+        
+    }
+};
